6d92aacf787f0e76ccf81206a876fb2f586d0777
Pragat28 <pragatibajajj@gmail.com>
2026-05-19 17:51:17 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://medilink-j44r.onrender.com", // ✅ FIXED
+  baseURL: "https://medilink-j44r.onrender.com/api", // ✅ FIXED
 });
 
 api.interceptors.request.use(config => {
